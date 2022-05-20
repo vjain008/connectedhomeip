85b095dc6bb32fce9d9b9fbed9cce0c93b6511a9
Vinod Jain <VinodKumar_Jain@comcast.com>
2022-05-20 12:10:24 -0700

    Launching Apps like YouTube/Prime on Hisense(Comcast) using RDKShell.

--- examples/tv-app/linux/include/content-launcher/ContentLauncherManager.cpp
+++ examples/tv-app/linux/include/content-launcher/ContentLauncherManager.cpp
@@ -53,22 +53,84 @@ void ContentLauncherManager::HandleLaunchUrl(CommandResponseHelper<LaunchRespons
     string displayStringString(displayString.data(), displayString.size());
 
     //Code for Demo#1
-	if(strcmp(displayStringString.c_str(), "demo") == 0)
-	{
-		string command("curl -H \"Authorization: Bearer `/usr/bin/WPEFrameworkSecurityUtility | cut -d'\"' -f 4`\" -d '{\"jsonrpc\": \"2.0\",\"id\": 42,\"method\": \"org.rdk.RDKShell.1.launch\",\"params\": {\"callsign\": \"demo\",\"type\": \"HtmlApp\",\"version\": \"1.0\",\"uri\":\"");
-		command.append(contentUrlString);
-		command.append("\",\"topmost\": true,\"focus\": true}}' http://127.0.0.1:9998/jsonrpc");
-		
-		ChipLogProgress(Zcl, "ContentLauncherManager::command = %s", command.c_str());
-		
-		system(command.c_str());
+	if (contentUrlString.find("https") != std::string::npos) {
+		if(strcmp(displayStringString.c_str(), "demo") == 0)
+		{
+			string command("curl -H \"Authorization: Bearer `/usr/bin/WPEFrameworkSecurityUtility | cut -d'\"' -f 4`\" -d '{\"jsonrpc\": \"2.0\",\"id\": 42,\"method\": \"org.rdk.RDKShell.1.launch\",\"params\": {\"callsign\": \"demo\",\"type\": \"HtmlApp\",\"version\": \"1.0\",\"uri\":\"");
+			command.append(contentUrlString);
+			command.append("\",\"topmost\": true,\"focus\": true}}' http://127.0.0.1:9998/jsonrpc");
+			ChipLogProgress(Zcl, "ContentLauncherManager::command = %s", command.c_str());
+			system(command.c_str());
+		}
+		else if(strcmp(displayStringString.c_str(), "destroy") == 0)
+		{
+			string command("curl -H \"Authorization: Bearer `/usr/bin/WPEFrameworkSecurityUtility | cut -d'\"' -f 4`\" -d '{\"jsonrpc\":\"2.0\",\"id\":\"3\",\"method\": \"org.rdk.RDKShell.1.destroy\", \"params\": {\"callsign\": \"demo\"}}' http://127.0.0.1:9998/jsonrpc");
+			ChipLogProgress(Zcl, "ContentLauncherManager::command = %s", command.c_str());
+			system(command.c_str());
+		}
 	}
-	else if(strcmp(displayStringString.c_str(), "destroy") == 0)
+	//Code for Demo#2
+	else
 	{
-		string command("curl -H \"Authorization: Bearer `/usr/bin/WPEFrameworkSecurityUtility | cut -d'\"' -f 4`\" -d '{\"jsonrpc\":\"2.0\",\"id\":\"3\",\"method\": \"org.rdk.RDKShell.1.destroy\", \"params\": {\"callsign\": \"demo\"}}' http://127.0.0.1:9998/jsonrpc");
-		ChipLogProgress(Zcl, "ContentLauncherManager::command = %s", command.c_str());
-		
-		system(command.c_str());
+		if(strcmp(contentUrlString.c_str(), "Netflix") == 0)
+		{
+			if(strcmp(displayStringString.c_str(), "demo") == 0)
+			{
+				string command("curl -H \"Authorization: Bearer `/usr/bin/WPEFrameworkSecurityUtility | cut -d'\"' -f 4`\" -d '{\"jsonrpc\": \"2.0\",\"id\": 42,\"method\": \"org.rdk.RDKShell.1.launch\",\"params\": {\"callsign\": \"Netflix-0\",\"type\": \"\",\"version\": \"1.0\",\"uri\":\"\",\"visible\": true,\"focused\": true,\"topmost\": true,\"focus\": true}}' http://127.0.0.1:9998/jsonrpc");
+				system(command.c_str());
+			}
+			else if(strcmp(displayStringString.c_str(), "destroy") == 0)
+			{
+				string command("curl -H \"Authorization: Bearer `/usr/bin/WPEFrameworkSecurityUtility | cut -d'\"' -f 4`\" -d '{\"jsonrpc\":\"2.0\",\"id\":\"3\",\"method\": \"org.rdk.RDKShell.1.destroy\", \"params\": {\"callsign\": \"Netflix-0\"}}' http://127.0.0.1:9998/jsonrpc");
+				system(command.c_str());
+			}
+		}
+		else if(strcmp(contentUrlString.c_str(), "Disney") == 0)
+		{
+			if(strcmp(displayStringString.c_str(), "demo") == 0)
+			{
+				string command("curl -H \"Authorization: Bearer `/usr/bin/WPEFrameworkSecurityUtility | cut -d'\"' -f 4`\" -d '{\"jsonrpc\": \"2.0\",\"id\": 42,\"method\": \"org.rdk.RDKShell.1.launch\",\"params\": {\"callsign\": \"HtmlApp-0\",\"type\": \"\",\"version\": \"1.0\",\"uri\":\"https://cd-dmgz.bamgrid.com/bbd/prod/1.19.0.ironman.02/comcast_x1.html?partnerId=xglobal&lmt=0&us_privacy=1-N-\",\"visible\": true,\"focused\": true,\"topmost\": true,\"focus\": true}}' http://127.0.0.1:9998/jsonrpc");
+				system(command.c_str());
+			}
+			else if(strcmp(displayStringString.c_str(), "destroy") == 0)
+			{
+				string command("curl -H \"Authorization: Bearer `/usr/bin/WPEFrameworkSecurityUtility | cut -d'\"' -f 4`\" -d '{\"jsonrpc\":\"2.0\",\"id\":\"3\",\"method\": \"org.rdk.RDKShell.1.destroy\", \"params\": {\"callsign\": \"HtmlApp-0\"}}' http://127.0.0.1:9998/jsonrpc");
+				system(command.c_str());
+			}
+		}
+		else if(strcmp(contentUrlString.c_str(), "Prime") == 0)
+		{
+			if(strcmp(displayStringString.c_str(), "demo") == 0)
+			{
+				string command("curl -H \"Authorization: Bearer `/usr/bin/WPEFrameworkSecurityUtility | cut -d'\"' -f 4`\" -d '{\"jsonrpc\": \"2.0\",\"id\": 42,\"method\": \"org.rdk.RDKShell.1.launch\",\"params\": {\"callsign\": \"HtmlApp-0\",\"type\": \"\",\"version\": \"1.0\",\"uri\":\"https://avpk-a12ttfat8x8j46-0.api.amazonvideo.com/lrc-vending/html5/index.html?deviceTypeID=A12TTFAT8X8J46&deviceID=77e8c216a79287298f72f13332dbdc1ecea479e7&comcast-zipcode=66952&canCacheInitJs=false&lmt=0&us_privacy=1-N-\",\"visible\": true,\"focused\": true,\"topmost\": true,\"focus\": true}}' http://127.0.0.1:9998/jsonrpc");
+				system(command.c_str());
+			}
+			else if(strcmp(displayStringString.c_str(), "destroy") == 0)
+			{
+				string command("curl -H \"Authorization: Bearer `/usr/bin/WPEFrameworkSecurityUtility | cut -d'\"' -f 4`\" -d '{\"jsonrpc\":\"2.0\",\"id\":\"3\",\"method\": \"org.rdk.RDKShell.1.destroy\", \"params\": {\"callsign\": \"HtmlApp-0\"}}' http://127.0.0.1:9998/jsonrpc");
+				system(command.c_str());
+			}
+		}
+		else if(strcmp(contentUrlString.c_str(), "YouTube") == 0)
+		{
+			if(strcmp(displayStringString.c_str(), "demo") == 0)
+			{
+				string command("curl -H \"Authorization: Bearer `/usr/bin/WPEFrameworkSecurityUtility | cut -d'\"' -f 4`\" -d '{\"jsonrpc\": \"2.0\",\"id\": 42,\"method\": \"org.rdk.RDKShell.1.launch\",\"params\": {\"callsign\": \"Cobalt-0\",\"type\": \"Cobalt\",\"version\": \"1.0\",\"uri\":\"\",\"visible\": true,\"focused\": true,\"topmost\": true,\"focus\": true}}' http://127.0.0.1:9998/jsonrpc");
+				system(command.c_str());
+			}
+			else if(strcmp(displayStringString.c_str(), "destroy") == 0)
+			{
+				string command("curl -H \"Authorization: Bearer `/usr/bin/WPEFrameworkSecurityUtility | cut -d'\"' -f 4`\" -d '{\"jsonrpc\":\"2.0\",\"id\":\"3\",\"method\": \"org.rdk.RDKShell.1.destroy\", \"params\": {\"callsign\": \"Cobalt-0\"}}' http://127.0.0.1:9998/jsonrpc");
+				system(command.c_str());
+			}
+			else
+			{
+				string command("curl -H \"Authorization: Bearer `/usr/bin/WPEFrameworkSecurityUtility | cut -d'\"' -f 4`\" 'http://127.0.0.1:9998/jsonrpc' -d '{\"jsonrpc\": \"2.0\",\"id\": 4,\"method\": \"Cobalt-0.1.deeplink\", \"params\": {\"?launch=search&v=");
+				command.append(displayStringString.c_str());
+				command.append("&inApp=false\"}}' ; echo");
+				system(command.c_str());
+			}
+		}
 	}
 
     // TODO: Insert code here
